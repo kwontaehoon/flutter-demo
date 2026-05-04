12d5e3e7073c5ac55d11202915b3828f0e511aa2
gju04195 <gju04195@gmail.com>
2026-05-04 23:46:03 +0900

    ui: bottom_bar page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/plant_page.dart';
+import 'package:flutter_application_1/pages/bottom_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Medical App',
+      title: 'Flutter Demo App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
         fontFamily: "Poppins",
       ),
-      home: const PlantPage(),
+      home: const BottomBar(),
     );
   }
 }
---
+++ lib/pages/bottom_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/profile_page.dart';
+import 'package:flutter_application_1/pages/search_page.dart';
+import 'package:flutter_application_1/pages/wishlist_page.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = [
+    Center(child: HomePage()),
+    WishlistPage(),
+    Center(child: SearchPage()),
+    Center(child: ProfilePage()),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // extendBody: true,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: _selectedIndex == 1 ? null : const Text('BottomBar'),
+        backgroundColor: _selectedIndex == 1
+            ? Colors.transparent
+            : const Color(0xff008762),
+        elevation: _selectedIndex == 1 ? 0 : 4,
+      ),
+
+      body: _pages[_selectedIndex],
+
+      bottomNavigationBar: SizedBox(
+        // height: 102,
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: const Color(0xff008761),
+          unselectedItemColor: Colors.grey,
+          // iconSize: 28,
+          // selectedLabelStyle: const TextStyle(fontSize: 11, height: 2.0),
+          // unselectedLabelStyle: const TextStyle(fontSize: 20, height: 2.0),
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: '위시리스트'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: '검색'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: '내 정보'),
+          ],
+        ),
+      ),
+    );
+  }
+}
